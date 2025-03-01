a70c3b6f12225081326bc656d2a95281480f8a40
OscarBermudez38 <157708525+OscarBermudez38@users.noreply.github.com>
2025-03-01 15:45:02 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,17 +71,17 @@ def sugerir_sintomas(symptoms, available_symptoms):
         st.markdown(f" sintoma {symptom}")
         symptom_lower = traducir_texto([symptom])  # Corregir el síntoma actual
 
-        if symptom_lower in available_symptoms_lower:
-            st.session_state["symptoms_corrected"][symptom_lower] = available_symptoms_lower[symptom_lower]
-        elif symptom_lower in st.session_state["symptoms_corrected"]:
+        if symptom_lower[0] in available_symptoms_lower:
+            st.session_state["symptoms_corrected"][symptom_lower[0]] = available_symptoms_lower[symptom_lower[0]]
+        elif symptom_lower[0] in st.session_state["symptoms_corrected"]:
             continue  
         else:
-            closest_matches = difflib.get_close_matches(symptom_lower, available_symptoms_lower.keys(), n=3, cutoff=0.4)
+            closest_matches = difflib.get_close_matches(symptom_lower[0], available_symptoms_lower.keys(), n=3, cutoff=0.4)
             if closest_matches:
-                pending[symptom_lower] = closest_matches
+                pending[symptom_lower[0]] = closest_matches
             else:
                 st.warning(f"El síntoma '{symptom}' no está registrado y no se encontraron coincidencias.")
-                st.session_state["symptoms_corrected"][symptom_lower] = symptom  
+                st.session_state["symptoms_corrected"][symptom_lower[0]] = symptom  
 
     if pending:
         st.session_state["pending_corrections"] = pending
